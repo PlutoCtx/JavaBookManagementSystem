3c4dd14334c2c89520c0c2d7ff9c75c62cc47805
MaxChentx <ctx195467@163.com>
2025-01-12 23:20:26 +0800

    Initial commit

---
+++ src/main/java/com/example/JavaBookManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabookmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBookManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBookManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.controller;
+
+/**
+ * JavaBookManagementSystem
+ * 测试
+ *
+ * @author PlutoCtx ctx195467@163.com
+ * @version 2025/1/12 11:09
+ * @since JDK17
+ */
+ 
+public class DemoController {
+}
