84b31fd6524104e962c49610e7c14cf30a5c6c39
MaxChentx <ctx195467@163.com>
2025-01-12 23:20:51 +0800

    page start

--- src/main/java/com/example/controller/DemoController.java
+++ src/main/java/com/example/controller/DemoController.java
@@ -1,5 +1,8 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * JavaBookManagementSystem
  * 测试
@@ -8,6 +11,20 @@ package com.example.controller;
  * @version 2025/1/12 11:09
  * @since JDK17
  */
- 
+
+@Controller
 public class DemoController {
+
+
+    @RequestMapping("adminLogin")
+    public String redirect(String username,
+                           String password) {
+
+        System.out.println(username);
+        System.out.println(password);
+
+        return "admin-page";
+    }
+
+
 }
